36657e0a116fdf37d887c40b87d28dd743ff07e5
Yosuke Mizutani <mogproj@gmail.com>
2016-09-24 21:05:59 -0400

    multiple editors

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@
 import * as vscode from "vscode";
 
 import { removeSelection, hasSelectedText } from "./util/selectionUtil";
+import { GlobalMarkController } from "./markController";
 import { joinLines } from "./command/join";
 import { toggleLetterCase } from "./command/letterCase";
 import { selectRectangle } from "./command/selection";
@@ -15,9 +16,6 @@ import TextEditorEdit = vscode.TextEditorEdit;
 import executeCommand = vscode.commands.executeCommand;
 
 
-let inMarkMode = false;
-let keepMark = 0;  // todo: refactor
-
 const supportedCursorMoves: string[] = [
     "cursorUp", "cursorDown", "cursorLeft", "cursorRight", "cursorHome", "cursorEnd",
     "cursorWordLeft", "cursorWordRight", "cursorPageDown", "cursorPageUp", "cursorTop", "cursorBottom"
@@ -27,10 +25,12 @@ export function activate(context: vscode.ExtensionContext) {
     type Cmd = [string, { (): void }];
     type EditCmd = [string, { (t: TextEditor, e: TextEditorEdit): void }];
 
+    const mc = new GlobalMarkController();
+
     // Prepare non-edit command definitions
     let commands: Cmd[] = [
-        ["mog.editor.action.enterMarkMode", () => enterMarkMode(Window.activeTextEditor)],
-        ["mog.editor.action.exitMarkMode", () => exitMarkMode(Window.activeTextEditor)],
+        ["mog.editor.action.enterMarkMode", () => mc.enter(Window.activeTextEditor)],
+        ["mog.editor.action.exitMarkMode", () => mc.exit(Window.activeTextEditor)],
         ["mog.editor.action.clipboardCopyAction", () => clipboardCopyAction(Window.activeTextEditor)],
         ["mog.editor.action.clipboardCutAction", () => executeCommand("editor.action.clipboardCutAction")],
         ["mog.editor.action.commentLine", commentLine],
@@ -39,16 +39,9 @@ export function activate(context: vscode.ExtensionContext) {
     ];
 
     // cursor moves
-    supportedCursorMoves.map(s => {
-        const sl = s + "Select";
-        return [[s, () => inMarkMode ? sl : s], [sl, () => sl]];
-    }).forEach(xss => {
-        xss.forEach((xs: [string, { (): string }]) =>
-            commands.push(["mog." + xs[0], () => {
-                executeCommand(xs[1]());
-                ++keepMark;
-            }])
-        )
+    supportedCursorMoves.forEach(s => {
+        commands.push(["mog." + s, () => mc.moveCursor(Window.activeTextEditor, s, false)]);
+        commands.push(["mog." + s + "Select", () => mc.moveCursor(Window.activeTextEditor, s, true)]);
     });
 
     // Prepare edit command definitions
@@ -65,35 +58,10 @@ export function activate(context: vscode.ExtensionContext) {
         .forEach(r => context.subscriptions.push(r));
 
     // Subscribe listeners
-    Window.onDidChangeTextEditorSelection(resetMarkMode);
+    Window.onDidChangeTextEditorSelection(ev => mc.reset(Window.activeTextEditor));
 
     console.log("Activated extension: mog-vscode");
 }
 
 export function deactivate() {
-}
-
-// Helper functions
-function resetMarkMode(ev: vscode.TextEditorSelectionChangeEvent): void {
-    if (inMarkMode) {
-        if (keepMark > 0) {
-            --keepMark;
-        } else {
-            inMarkMode = false;
-        }
-    } else {
-        keepMark = 0;
-    }
-}
-
-// Commands
-function enterMarkMode(t: TextEditor): void {
-    if (hasSelectedText(t) && !inMarkMode) { ++keepMark; }
-    removeSelection(t);
-    inMarkMode = !inMarkMode;
-}
-
-function exitMarkMode(t: TextEditor): void {
-    removeSelection(t);
-    inMarkMode = false;
 }
---
+++ src/markController.ts
@@ -0,0 +1,82 @@
+"use strict";
+
+import * as vscode from "vscode";
+
+import { removeSelection, hasSelectedText } from "./util/selectionUtil";
+
+import TextEditor = vscode.TextEditor;
+import executeCommand = vscode.commands.executeCommand;
+
+
+class EditorMarkController {
+    constructor(
+        private textEditor: TextEditor,
+        private isMarked: boolean = false,
+        private eventCount: number = 0
+    ) { }
+
+    public reset(): void {
+        if (this.isMarked) {
+            if (this.eventCount > 0) {
+                --this.eventCount;
+            } else {
+                this.isMarked = false;
+            }
+        } else {
+            this.eventCount = 0;
+        }
+    }
+
+    public incrementEvnetCount(): void {
+        ++this.eventCount;
+    }
+
+    public enter(): void {
+        if (hasSelectedText(this.textEditor) && !this.isMarked) this.incrementEvnetCount();
+        removeSelection(this.textEditor);
+        this.isMarked = !this.isMarked;
+    }
+
+    public exit(): void {
+        removeSelection(this.textEditor);
+        this.isMarked = false;
+    }
+
+    public moveCursor(command: string, isSelect: boolean): void {
+        const cmd = (isSelect || this.isMarked) ? command + "Select" : command;
+        executeCommand(cmd);
+        this.incrementEvnetCount();
+    }
+}
+
+export class GlobalMarkController {
+    private editorMarkControllers: Map<TextEditor, EditorMarkController> = new Map();
+
+    constructor() { }
+
+    private getMarkController(t: TextEditor): EditorMarkController {
+        if (this.editorMarkControllers.has(t)) {
+            return this.editorMarkControllers.get(t);
+        } else {
+            const mc = new EditorMarkController(t);
+            this.editorMarkControllers.set(t, mc);
+            return mc;
+        }
+    }
+
+    public enter(t: TextEditor): void {
+        this.getMarkController(t).enter();
+    }
+
+    public exit(t: TextEditor): void {
+        this.getMarkController(t).exit();
+    }
+
+    public reset(t: TextEditor): void {
+        this.getMarkController(t).reset();
+    }
+
+    public moveCursor(t: TextEditor, command: string, isSelect: boolean): void {
+        this.getMarkController(t).moveCursor(command, isSelect);
+    }
+}
