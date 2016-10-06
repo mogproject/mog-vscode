0189bf2039615a1f1aa13ae82fb852e5b8bfb61d
Yosuke Mizutani <mogproj@gmail.com>
2016-10-06 05:23:52 -0400

    Execute saveAll command before external commands

--- src/command/externalCommand.ts
+++ src/command/externalCommand.ts
@@ -15,25 +15,27 @@ export class ExternalCommand {
     }
 
     public execute(id: string) {
-        const cmd_id = "cmd_" + id
-        const cmd = this.config[cmd_id];
-        this.output.clear();
-        if (!cmd) {
-            this.output.appendLine("Command not defined: " + this.configPrefix + "." + cmd_id);
-            // todo: add help messages
-        } else {
-            this.output.appendLine("Executing: " + cmd);
-            try {
-                const proc = child_process.spawn(cmd, [], {
-                    shell: true,
-                    cwd: vscode.workspace.rootPath,
-                });
-                proc.stdout.on("data", (data: any)=>this.output.append(data.toString()));
-                proc.stderr.on("data", (data: any)=>this.output.append(data.toString()));
-            } catch (error) {
-                this.output.appendLine(error);
+        vscode.commands.executeCommand("workbench.action.files.saveAll").then(() => {
+            const cmd_id = "cmd_" + id
+            const cmd = this.config[cmd_id];
+            this.output.clear();
+            if (!cmd) {
+                this.output.appendLine("Command not defined: " + this.configPrefix + "." + cmd_id);
+                // todo: add help messages
+            } else {
+                this.output.appendLine("Executing: " + cmd);
+                try {
+                    const proc = child_process.spawn(cmd, [], {
+                        shell: true,
+                        cwd: vscode.workspace.rootPath,
+                    });
+                    proc.stdout.on("data", (data: any)=>this.output.append(data.toString()));
+                    proc.stderr.on("data", (data: any)=>this.output.append(data.toString()));
+                } catch (error) {
+                    this.output.appendLine(error);
+                }
             }
-        }
-        this.output.show();
+            this.output.show();
+        })
     }
 }
